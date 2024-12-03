9bc97fc985f3032e294be1b6b89a8ec65664294e
Michael Baucum <32890936+InterpolatedPixel@users.noreply.github.com>
2024-12-02 21:31:52 -0800

    Michael Day 1

---
+++ Michael/Day-1/Program.cs
@@ -0,0 +1,35 @@
+﻿// File
+var inputFilePath = "input.txt";
+var lines = File.ReadAllLines(inputFilePath);
+
+// Numbers
+List<int> numbers1 = new List<int>();
+List<int> numbers2 = new List<int>();
+List<int> differences = new List<int>();
+
+
+// Populate the lists
+foreach (var line in lines)
+{
+    var parts = line.Split("   ");
+    numbers1.Add(int.Parse(parts[0]));
+    numbers2.Add(int.Parse(parts[1]));
+}
+
+// Sort the Lists
+numbers1.Sort();
+numbers2.Sort();
+
+
+// Populate the differences
+for (int i = 0; i <= numbers1.Count - 1; i++)
+{
+    differences.Add(Math.Abs(numbers1[i] - numbers2[i]));
+}
+
+
+// Get the sum of all the differences.
+var differencesSum = differences.Sum();
+
+// Print the sum
+Console.WriteLine(differencesSum);
